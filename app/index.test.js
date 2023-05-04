143da103ac6c017054bbc317e2cd9b0b783e79e5
Marco Fratini <marcofratini@outlook.com>
2023-05-04 18:41:59 +0200

    Add second testing condition to newLength function

--- app/index.test.js
+++ app/index.test.js
@@ -11,4 +11,15 @@ describe("Given the function newLength", () => {
       expect(resultReturned).toBe(expectedResult);
     });
   });
+
+  describe("When given the array of 5, 10 , 15, 20, 25", () => {
+    test("Then it should return 5", () => {
+      const array = [5, 10, 15, 20, 25];
+
+      const expectedResult = 5;
+      const resultReturned = index.newLength(array);
+
+      expect(resultReturned).toBe(expectedResult);
+    });
+  });
 });
